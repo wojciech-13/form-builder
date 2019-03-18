31ed25601870f572a7d462c723e26d4a14e5f13f
Wojciech Borkowski <wojciechborkowski13@gmail.com>
2019-03-19 00:09:27 +0100

    Make possibility to add sub-forms connected to parrent forms

--- src/Form/FormComponent.js
+++ src/Form/FormComponent.js
@@ -1,39 +1,76 @@
 import React from 'react';
 import "./FormStyles.css"
+import uniqueId from "lodash/uniqueId";
 
+class FormComponent extends React.Component {
 
-function FormComponent(props){
+    constructor(props){
+        super(props);
+        this.state = {
+            id: this.props.id,
+            subForms: []
+        };
+        this.addSubForms = this.addSubForms.bind(this);
 
+    }
 
-    return (
-        <div className="formContainer">
+    addSubForms() {
+        this.setState(prevState => {
+            return   (
+                {
+                ...prevState,
+                subForms: [...prevState.subForms, {id: uniqueId()}]
+                }
+            );
+        });
+    }
 
-            <div>
-            <label> <span className="span">Condition</span>
-                <select className="inputs" ></select>
-            </label>
+    render() {
+
+        const subForms = this.state.subForms.map(subForm => {
+            return (
+                <FormComponent
+                    key={subForm.id}
+                    id={subForm.id}
+                />
+            )
+        })
+
+        return (
+            <>
+            <div className="formContainer">
+                id = {this.props.id}  {/*  pomocnicze id*/}
+                <div>
+                <label> <span className="span">Condition</span>
+                    <select className="inputs" ></select>
+                </label>
+                </div>
+
+                <label> <span className="span">Question</span>
+                    <input name="question" className="inputs" type="text"/>
+                </label>
+
+                <label> <span className="span">Type </span>
+                    <select className="inputs">
+                        <option>Yes / No</option>
+                        <option>Text</option>
+                        <option>Number</option>
+                    </select>
+                </label>
+
+                <div className="buttonsContainer">
+                    <button onClick={this.addSubForms}>Add Sub-Form</button>
+                    <button>Delete</button>
+                </div>
+                
             </div>
-
-            <label> <span className="span">Question</span>
-                <input name="question" className="inputs" type="text"/>
-            </label>
-
-            <label> <span className="span">Type </span>
-                <select className="inputs">
-                    <option>Yes / No</option>
-                    <option>Text</option>
-                    <option>Number</option>
-                </select>
-            </label>
-
-            <div className="buttonsContainer">
-                <button name="addSubInput">Add Sub-Input</button>
-                <button>Delete</button>
+            <div className="subFormContainer">
+            {subForms}
             </div>
+            </>
 
-        </div>
-
-    );
+        );
+    }
 }
 
 export default FormComponent
